cf981b609698050fd6408b0ea25d4abda365eecd
kakandalutfi <m.lutfihabibunnazar@gmail.com>
2024-07-31 13:48:25 +0700

    penambahan fitur

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
-window.onscroll = function() {
+// Navbar Fixed
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
-
+    const toTop = document.querySelector('#to-top');
+  
     if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
+      header.classList.add('navbar-fixed');
+      toTop.classList.remove('hidden');
+      toTop.classList.add('flex');
     } else {
-        header.classList.remove('navbar-fixed');
+      header.classList.remove('navbar-fixed');
+      toTop.classList.remove('flex');
+      toTop.classList.add('hidden');
     }
-};
-
-const hamburger = document.querySelector ('#hamburger');
-const navMenu = document.querySelector('#nav-menu'); 
-
-hamburger.addEventListener('click', function() {
-     hamburger.classList.toggle('hamburger-active');
-     navMenu.classList.toggle('hidden');
-});
+  };
+  
+  // Hamburger
+  const hamburger = document.querySelector('#hamburger');
+  const navMenu = document.querySelector('#nav-menu');
+  
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+  });
+  
+  // Klik di luar hamburger
+  window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+      hamburger.classList.remove('hamburger-active');
+      navMenu.classList.add('hidden');
+    }
+  });x
+  
